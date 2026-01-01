2bc5eb27b116ff3f4620690033c8c4762628c533
milkandhoney995 <harada.g17@gmail.com>
2026-01-01 22:18:03 +0900

    import Referee from '@/components/chess/Piece/Piece'

--- src/app/chess/page.tsx
+++ src/app/chess/page.tsx
@@ -1,9 +1,9 @@
-import Piece from '@/components/chess/Piece/Piece'
 import styles from '@/app/page.module.scss'
+import Referee from '@/components/chess/Piece/Piece'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Piece />
+      <Referee />
     </main>
   )
 }
